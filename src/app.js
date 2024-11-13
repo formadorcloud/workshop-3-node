17db0d531486d956d2618f2044311f9ae8d0523f
formadorcloud <formador.cloud@makaia.org>
2024-11-13 15:52:47 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
-app.get('/status', (req, res) => {
- res.json({ status: 'API is working!' });
+
+// Middleware para parsear JSON
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).send('Bienvenido a la API');
 });
+
+app.get('/status', (req, res) => {
+  res.status(200).json({ status: 'API funcionando correctamente' });
+});
+
 module.exports = app;
