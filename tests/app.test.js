f3283e3d57b12fb27f7e105e7995e814e08940c5
formadorcloud <formador.cloud@makaia.org>
2024-11-13 15:32:59 -0500

    Create app.test.js

---
+++ tests/app.test.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const app = require('../src/app');
+
+test('GET /status should return API is working', async () => {
+ const response = await request(app).get('/status');
+ expect(response.statusCode).toBe(200);
+ expect(response.body).toHaveProperty('status', 'API is working!');
+});
